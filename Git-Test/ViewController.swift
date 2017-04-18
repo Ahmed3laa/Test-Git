d3d45b78ea1ea22aeb04cca6b0c1b00b984eaf4c
Ahmed Alaa <ahmedalaa@ahmeds-mac.local>
2017-04-18 15:06:06 +0200

    Initial Commit

---
+++ Git-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git-Test
+//
+//  Created by Ahmed Alaa on 4/18/17.
+//  Copyright © 2017 Ahmed Alaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
